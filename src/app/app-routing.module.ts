a2bcacc8518c5101323b2ff2a39a86bf00a906fa
Alex <alextjd1@gmail.com>
2020-02-11 21:18:50 +0100

    Add the core routing functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MattersContainerComponent } from './matters/matters-container/matters-container.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'matters' },
+  {
+    path: 'matters',
+    component: MattersContainerComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
