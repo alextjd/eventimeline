8e5f9febb71e75fb0d39d5144584074451a174f3
Alex <alextjd1@gmail.com>
2020-02-12 21:28:58 +0100

    Implement the matter service
    This service communicates with the backend to get
    the historic data. It also pases this data to adapt
    it to what the components need, and in the process
    gets rid of the snake casing found in some of the
    data objects of the backend.
    
    The components will not access the historical data
    directly. They will access the matters data, which
    is simply parsed historical data.

--- src/app/shared/services/matter.service.ts
+++ src/app/shared/services/matter.service.ts
@@ -1,9 +1,55 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+
+import {
+  Matter,
+  History,
+  HistoryData,
+  MatterType
+} from '../interfaces/matter.interface';
+
+const url = 'http://history.muffinlabs.com/date';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MatterService {
+  history$: Observable<History>;
+  matters$: Subject<Matter[]> = new Subject<Matter[]>();
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.history$ = this.getAllHistory();
+
+    this.history$.subscribe((history: History) => {
+      const matters: Matter[] = this.historyToMatters(history);
+      this.matters$.next(matters);
+    });
+  }
+
+  getAllHistory(): Observable<History> {
+    return this.http.get<History>(url);
+  }
+
+  getAllMatters() {
+    return this.getAllHistory();
+  }
+
+  historyToMatters(history: History): Matter[] {
+    const parsedMatters: Matter[] = [];
+    const historyData: HistoryData = history.data;
+    for (const matterType of Object.keys(historyData)) {
+      for (const matter of historyData[matterType]) {
+        parsedMatters.push({
+          year: matter.year,
+          date: matter.date,
+          type: MatterType[matterType],
+          text: matter.text,
+          html: matter.html,
+          noYearHtml: matter.no_year_html
+        });
+      }
+    }
+    return parsedMatters;
+  }
 }
