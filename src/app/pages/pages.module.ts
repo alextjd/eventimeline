e7c6fbea325c92830b992cf840a7d7b10e972f8a
Alex <alextjd1@gmail.com>
2020-02-12 20:09:50 +0100

    Create shared module and add it to pages

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { PagesComponent } from './pages/pages.component';
 import { MattersModule } from '../matters/matters.module';
 import { LayoutModule } from '../layout/layout.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  imports: [CommonModule, LayoutModule, MattersModule],
+  imports: [CommonModule, LayoutModule, MattersModule, SharedModule],
   declarations: [PagesComponent],
   exports: [PagesComponent]
 })
