d5ba582e3a70344ee47e085a142c7c7e57b88a26
Alex <alextjd1@gmail.com>
2020-02-12 19:44:14 +0100

    Export the sidenav component

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,12 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidenavComponent } from './sidenav/sidenav.component';
 
-
-
 @NgModule({
+  imports: [CommonModule],
   declarations: [SidenavComponent],
-  imports: [
-    CommonModule
-  ]
+  exports: [SidenavComponent]
 })
-export class LayoutModule { }
+export class LayoutModule {}
