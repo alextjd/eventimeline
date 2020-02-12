d0b5698aa677f4bbf0fd4e101913e2a245e11e03
Alex <alextjd1@gmail.com>
2020-02-12 20:02:36 +0100

    Add the matter input to matter item

--- src/app/matters/matter-item/matter-item.component.ts
+++ src/app/matters/matter-item/matter-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-matter-item',
@@ -6,10 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./matter-item.component.scss']
 })
 export class MatterItemComponent implements OnInit {
+  @Input() matter: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
