7b6ef6d3724a17df5928c9a51462bce006ad5ff5
Alex <alextjd1@gmail.com>
2020-02-14 20:13:17 +0100

    Create the reusable module and the select component

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,9 +4,16 @@ import { PagesComponent } from './pages/pages.component';
 import { MattersModule } from '../matters/matters.module';
 import { LayoutModule } from '../layout/layout.module';
 import { SharedModule } from '../shared/shared.module';
+import { ReusableModule } from '../reusable/reusable.module';
 
 @NgModule({
-  imports: [CommonModule, LayoutModule, MattersModule, SharedModule],
+  imports: [
+    CommonModule,
+    LayoutModule,
+    MattersModule,
+    SharedModule,
+    ReusableModule
+  ],
   declarations: [PagesComponent],
   exports: [PagesComponent]
 })
