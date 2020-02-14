1ab783bf231d9c1be74e64fbbf280cb663a847ab
Alex <alextjd1@gmail.com>
2020-02-14 20:16:32 +0100

    Add the base selectcomponent template and update module imports

--- src/app/matters/matters.module.ts
+++ src/app/matters/matters.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 import { MattersContainerComponent } from './matters-container/matters-container.component';
 import { MatterItemComponent } from './matter-item/matter-item.component';
 import { MattersFilterComponent } from './matters-filter/matters-filter.component';
+import { ReusableModule } from '../reusable/reusable.module';
 
 @NgModule({
-  declarations: [MattersContainerComponent, MatterItemComponent, MattersFilterComponent],
-  imports: [CommonModule],
+  declarations: [
+    MattersContainerComponent,
+    MatterItemComponent,
+    MattersFilterComponent
+  ],
+  imports: [CommonModule, ReusableModule],
   exports: [MattersContainerComponent]
 })
 export class MattersModule {}
