c78f6c768169a3747fd4a2f8f747d0c269f130e6
Alex <alextjd1@gmail.com>
2020-02-14 21:02:16 +0100

    Add basic reactive forms config

--- src/app/matters/matters-filter/matters-filter.component.ts
+++ src/app/matters/matters-filter/matters-filter.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { MatterType } from 'src/app/shared/interfaces/matter.interface';
 
 @Component({
   selector: 'app-matters-filter',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./matters-filter.component.scss']
 })
 export class MattersFilterComponent implements OnInit {
+  selectConfig = {
+    options: [MatterType.Deaths, MatterType.Births, MatterType.Events],
+    title: 'Type'
+  };
 
-  constructor() { }
+  filterForm = new FormGroup({
+    type: new FormControl(''),
+    startDate: new FormControl(''),
+    endDate: new FormControl('')
+  });
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.onFilterChange();
   }
 
+  onFilterChange() {
+    this.filterForm.valueChanges.subscribe();
+  }
 }
