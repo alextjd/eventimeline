dda3d26a836613ef8bc5c82ceea7fa88464b6e86
Alex <alextjd1@gmail.com>
2020-02-14 21:59:49 +0100

    Remove select component until a situation where it helps happens

--- src/app/reusable/reusable.module.ts
+++ src/app/reusable/reusable.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SelectComponent } from './select/select.component';
 
 @NgModule({
-  declarations: [SelectComponent],
+  declarations: [],
   imports: [CommonModule],
-  exports: [SelectComponent]
+  exports: []
 })
 export class ReusableModule {}
