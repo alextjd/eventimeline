bda303808964d3817275b0116d91d6abf54fdf9a
Alex <alextjd1@gmail.com>
2020-02-15 13:46:21 +0100

    Implement date validation
    Reactive form data validation is completed. Both
    dates must belong to the current month we are in and
    start date cannot be greater than end date. If
    this requirements are met, the dates are valid.

--- src/app/core/validators/date.validator.ts
+++ src/app/core/validators/date.validator.ts
@@ -1,17 +1,24 @@
 import { FormControl } from '@angular/forms';
-import { isSameMonth } from 'date-fns';
+import { isSameMonth, isAfter } from 'date-fns';
+import { ErrorMsgs } from 'src/app/shared/constants/error-messages.constants';
 
 export function DateValidator(): any {
   return (control: FormControl): { [key: string]: any } => {
     const start: string = control.get('startDate').value;
     const end: string = control.get('endDate').value;
     const today: Date = new Date();
-    if (
-      !isSameMonth(new Date(start), today) ||
-      !isSameMonth(new Date(end), today)
-    ) {
-      return { startDate: 'Invalid date', endDate: 'Invalid date' };
+    const res: { [x: string]: string } = {};
+
+    if (!isSameMonth(new Date(start), today)) {
+      res.startDate = ErrorMsgs.outOfMonthDate;
     }
-    return null;
+    if (!isSameMonth(new Date(end), today)) {
+      res.endDate = ErrorMsgs.outOfMonthDate;
+    }
+    if (isAfter(new Date(start), new Date(end))) {
+      res.startDate = ErrorMsgs.startAfterEnd;
+      res.endDate = ErrorMsgs.startAfterEnd;
+    }
+    return Object.keys(res).length > 0 ? res : null;
   };
 }
