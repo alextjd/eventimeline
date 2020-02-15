66b4fff3f9b8c23e8718b032d5cf71c14abf7c07
Alex <alextjd1@gmail.com>
2020-02-15 12:53:29 +0100

    Initialize datepickers to now and minor changes

--- src/app/core/validators/date.validator.ts
+++ src/app/core/validators/date.validator.ts
@@ -5,6 +5,13 @@ export function DateValidator(): any {
   return (control: FormControl): { [key: string]: any } => {
     const start: string = control.get('startDate').value;
     const end: string = control.get('endDate').value;
+    const today: Date = new Date();
+    if (
+      !isSameMonth(new Date(start), today) ||
+      !isSameMonth(new Date(end), today)
+    ) {
+      return { startDate: 'Invalid date', endDate: 'Invalid date' };
+    }
     return null;
   };
 }
--- src/app/matters/matters-filter/matters-filter.component.ts
+++ src/app/matters/matters-filter/matters-filter.component.ts
@@ -4,6 +4,7 @@ import { MatterType } from 'src/app/shared/interfaces/matter.interface';
 import { MatterFilter } from 'src/app/shared/interfaces/filter.interface';
 import { Validators } from '@angular/forms';
 import { DateValidator } from 'src/app/core/validators/date.validator';
+import { format } from 'date-fns';
 
 @Component({
   selector: 'app-matters-filter',
@@ -20,8 +21,8 @@ export class MattersFilterComponent implements OnInit {
   filterForm = new FormGroup(
     {
       type: new FormControl(''),
-      startDate: new FormControl(''),
-      endDate: new FormControl('')
+      startDate: new FormControl(format(new Date(), 'yyyy-MM-dd')),
+      endDate: new FormControl(format(new Date(), 'yyyy-MM-dd'))
     },
     DateValidator()
   );
