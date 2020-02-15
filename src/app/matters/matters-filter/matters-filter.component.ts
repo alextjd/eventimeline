d1a36d0ee19e1c464781d213a17d4357775389d7
Alex <alextjd1@gmail.com>
2020-02-15 12:05:10 +0100

    Add the date validator to the form

--- src/app/matters/matters-filter/matters-filter.component.ts
+++ src/app/matters/matters-filter/matters-filter.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { MatterType } from 'src/app/shared/interfaces/matter.interface';
 import { MatterFilter } from 'src/app/shared/interfaces/filter.interface';
+import { Validators } from '@angular/forms';
+import { DateValidator } from 'src/app/core/validators/date.validator';
 
 @Component({
   selector: 'app-matters-filter',
@@ -15,11 +17,14 @@ export class MattersFilterComponent implements OnInit {
     options: [MatterType.Deaths, MatterType.Births, MatterType.Events]
   };
 
-  filterForm = new FormGroup({
-    type: new FormControl(''),
-    startDate: new FormControl(''),
-    endDate: new FormControl('')
-  });
+  filterForm = new FormGroup(
+    {
+      type: new FormControl(''),
+      startDate: new FormControl(''),
+      endDate: new FormControl('')
+    },
+    DateValidator()
+  );
 
   constructor() {}
 
