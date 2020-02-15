2da142f2108f86abe996964d7f96697d2b45d141
Alex <alextjd1@gmail.com>
2020-02-15 19:47:03 +0100

    Create the pagination component

--- src/app/reusable/reusable.module.ts
+++ src/app/reusable/reusable.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorHelpComponent } from './error-help/error-help.component';
+import { PaginationComponent } from './pagination/pagination.component';
 
 @NgModule({
-  declarations: [ErrorHelpComponent],
+  declarations: [ErrorHelpComponent, PaginationComponent],
   imports: [CommonModule],
-  exports: [ErrorHelpComponent]
+  exports: [ErrorHelpComponent, PaginationComponent]
 })
 export class ReusableModule {}
