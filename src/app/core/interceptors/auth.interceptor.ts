af4582509bdc4d08c176d1ba8edd56e101839a07
Alex <alextjd1@gmail.com>
2020-03-22 19:25:39 +0100

    Implement the authentication logic
    The authentication process is simple in this
    case: the headers of the request must specify
    who rules. If the client specifies the server
    who rules, it will prove worthy of accessing the
    resources of the api.

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -7,12 +7,17 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+const key = 'cbcprls';
+
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-
   constructor() {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    request.headers.set('cbcprls', key);
     return next.handle(request);
   }
 }
