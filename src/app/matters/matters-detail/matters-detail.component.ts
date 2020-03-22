89e0de5f47a2916cd8724394c2402efe345e5808
Alex <alextjd1@gmail.com>
2020-03-22 22:37:31 +0100

    Implement the matters detail component

---
+++ src/app/matters/matters-detail/matters-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-matters-detail',
+  templateUrl: './matters-detail.component.html',
+  styleUrls: ['./matters-detail.component.scss']
+})
+export class MattersDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/matters/matters.module.ts
+++ src/app/matters/matters.module.ts
@@ -7,12 +7,14 @@ import { ReusableModule } from '../reusable/reusable.module';
 import { MatterItemComponent } from './matter-item/matter-item.component';
 import { MattersContainerComponent } from './matters-container/matters-container.component';
 import { MattersFilterComponent } from './matters-filter/matters-filter.component';
+import { MattersDetailComponent } from './matters-detail/matters-detail.component';
 
 @NgModule({
   declarations: [
     MattersContainerComponent,
     MatterItemComponent,
-    MattersFilterComponent
+    MattersFilterComponent,
+    MattersDetailComponent
   ],
   imports: [
     CommonModule,
